57ca7fa5a9dc9d0712cb21912a8454a07d207eeb
Courtney Hampton <jaye0888@gmail.com>
2023-01-17 15:20:52 +0100

    Server running and persists on Mongo

---
+++ src/api/blogs/model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const blogsSchema = new Schema(
+  {
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    age: { type: Number, required: true },
+    readTime: {
+      value: { type: Number },
+      unit: { type: String },
+    },
+    author: {
+      name: { type: String },
+      avatar: { type: String },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("Blog", blogsSchema);
